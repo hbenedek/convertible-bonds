0421c388c97f5cd1d21151a579d34acf86e02059
Hilda Abigél Horváth <hilda.horvath@epfl.ch>
2022-04-30 22:53:27 +0200

    model calibration

---
+++ calibration.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+from model import *
+import statsmodels.formula.api as smf
+
+# data 
+data = pd.read_csv('data.txt', header=0,  sep="\t")
+print(data.columns)
+
+# assumptions: r is constant, dividends each year with delta
+S_0 = 175
+
+# using put call parity
+# call - put - S_0
+
+data["val"]  = data["call"].values - data["put"].values - np.ones(len(data["call"])) * S_0
+
+# linear regression 
+model = smf.ols(formula='val ~ 1 + strike', data=data)
+results = model.fit()
+print(results.summary())
+
+disc_factor = -results.params[1]
+PVdividend = -results.params[0]
+
+# calculate r 
+r = -1 * np.log(disc_factor)
+print(r)
+
+# caluculate delta
+x = PVdividend / S_0
+delta = x / (1-x)
+print(delta)
+
+# calibrate model
+# 60 one-month period, p = 1/2
+
+D = np.exp(r/12) - np.sqrt(np.exp(r/6)-1)  
+U = 1/D
+print(U)
+
+BinomialModel("Johnson", delta=1/12, T=60, r=r, S_0=S_0, dividend_dates=[12,24,36,48,60], dividend_yield=delta, U=U, D=D)
