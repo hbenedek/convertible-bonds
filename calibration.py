b8494b286e6d617d12960e0ad7a2c316d9aabe31
Hilda Abigél Horváth <hilda.horvath@epfl.ch>
2022-05-01 10:22:48 +0200

    calibration output

--- calibration.py
+++ calibration.py
@@ -18,6 +18,7 @@ data["val"]  = data["call"].values - data["put"].values - np.ones(len(data["call
 # linear regression 
 model = smf.ols(formula='val ~ 1 + strike', data=data)
 results = model.fit()
+print("Result of the linear regression")
 print(results.summary())
 
 disc_factor = -results.params[1]
@@ -25,11 +26,13 @@ PVdividend = -results.params[0]
 
 # calculate r 
 r = -1 * np.log(disc_factor)
+print("Value of r:")
 print(r)
 
 # caluculate delta
 x = PVdividend / S_0
 delta = x / (1+x)
+print("Value of delta:")
 print(delta)
 
 # calibrate model
@@ -37,7 +40,9 @@ print(delta)
 
 D = np.exp(r/12) - np.sqrt(np.exp(r/6) - 1)  
 U = 1/D
+print("Value of D:")
 print(D)
+print("Value of U")
 print(U)
 
 BinomialModel("Johnson", delta=1/12, T=60, r=r, S_0=S_0, dividend_dates=[12,24,36,48,60], dividend_yield=delta, U=U, D=D)
