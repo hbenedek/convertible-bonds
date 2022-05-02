a53e523c0ba4e1f1675ca9b89bfcf93b972a7353
Benedek Harsanyi <hben.0204@gmail.com>
2022-05-02 16:53:03 +0200

    fix plot functions and add results

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from model import *
+import seaborn as sns
 
 
 # Constants definition (Bonds).
@@ -19,13 +20,13 @@ def solve_for_c(stock_model: BinomialModel, P0: float, type_bond: str, c_min: fl
 
     ind_c = None
     if type_bond == "mCB": 
-        prices = np.array([MandatoryConvertibleBond('', model=stock_model, alpha=ALPHA, beta=gamma, face_value=F, coupon_rate=c, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60]).calculate_price() for gamma_ in gammas])
+        prices = np.array([MandatoryConvertibleBond('', model=stock_model, alpha=ALPHA, beta=gamma, face_value=F, coupon_rate=c, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60]).calculate_price() for c in cs])
         ind_c = np.argmin(abs(prices - P0))
     elif type_bond == "CB":
-        prices = np.array([ConvertibleBond('', model=stock_model, face_value=F, coupon_rate=c, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60], gamma=gamma).calculate_price() for gamma_ in gammas]) 
+        prices = np.array([ConvertibleBond('', model=stock_model, face_value=F, coupon_rate=c, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60], gamma=gamma).calculate_price() for c in cs]) 
         ind_c = np.argmin(abs(prices - P0))
     else:
-        prices = np.array([callableCB('', model=stock_model, face_value=F, coupon_rate=c, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60], gamma=gamma, call_price=F_C).calculate_price() for gamma_ in gammas])
+        prices = np.array([callableCB('', model=stock_model, face_value=F, coupon_rate=c, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60], gamma=gamma, call_price=F_C).calculate_price() for c in cs])
         ind_c = np.argmin(abs(prices - P0))
 
     return cs[ind_c]
@@ -101,58 +102,87 @@ if __name__ == "__main__":
  
 
     # Question 5.4.
-    prices = []
-    cs = np.linspace(0.01, 0.99, num=1000)
+    prices1 = []
+    prices2 = []
+    prices3 = []
+    cs = np.linspace(0.01, 0.2, num=1000)
     for c in cs:
-        prices.append(callableCB('', model=model, face_value=F, coupon_rate=c, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60], gamma=GAMMA, call_price=F_C).calculate_price())
-        # prices.append(MandatoryConvertibleBond('', model=model, alpha=ALPHA, beta=BETA, face_value=F, coupon_rate=c, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60]).calculate_price())
-        # prices.append(ConvertibleBond('', model=model, face_value=F, coupon_rate=c, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60], gamma=GAMMA).calculate_price())
-    
-    # plt.plot(cs, prices)
-    # plt.xlabel("Coupon rate c")
-    # plt.ylabel("Initial price of the cCB")
-    # plt.grid()
-    #plt.show()
-    
-    prices = []
-    gammas = np.linspace(0.01, 30, num=1000)
+        prices1.append(callableCB('', model=model, face_value=F, coupon_rate=c, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60], gamma=GAMMA, call_price=F_C).calculate_price())
+        prices2.append(MandatoryConvertibleBond('', model=model, alpha=ALPHA, beta=BETA, face_value=F, coupon_rate=c, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60]).calculate_price())
+        prices3.append(ConvertibleBond('', model=model, face_value=F, coupon_rate=c, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60], gamma=GAMMA).calculate_price())
+
+    f, ax = plt.subplots(nrows=1, ncols=3, figsize=(21,7))
+    ax[0].plot(cs, prices1, label='callable')
+    ax[0].plot(cs, prices2, label='mandatory')
+    ax[0].plot(cs, prices3, label='vanilla')
+    ax[0].set_xlabel("Coupon rate c")
+    ax[0].set_ylabel("Initial price of the CB")
+    #ax[0].xaxis.set_tick_params(labelsize=18)
+    #ax[0].yaxis.set_tick_params(labelsize=18)
+    ax[0].legend()
+    ax[0].grid()
+
+
+    prices1 = []
+    prices2 = []
+    prices3 = []
+    gammas = np.linspace(0.01, 20, num=1000)
     for gamma_ in gammas:
-        # prices.append(MandatoryConvertibleBond('', model=model, alpha=ALPHA, beta=gamma_, face_value=F, coupon_rate=COUPON_RATE, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60]).calculate_price())
-        # prices.append(ConvertibleBond('', model=model, face_value=F, coupon_rate=COUPON_RATE, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60], gamma=gamma_).calculate_price())
-        prices.append(callableCB('', model=model, face_value=F, coupon_rate=COUPON_RATE, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60], gamma=gamma_, call_price=F_C).calculate_price())
-    
-    # plt.plot(gammas, prices)
-    # plt.xlabel(r"Conversion rate $\gamma$")
-    # plt.ylabel("Initial price of the cCB")
-    # plt.grid()
-    #plt.show() 
+        prices1.append(callableCB('', model=model, face_value=F, coupon_rate=COUPON_RATE, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60], gamma=gamma_, call_price=F_C).calculate_price())
+        prices2.append(MandatoryConvertibleBond('', model=model, alpha=ALPHA, beta=gamma_, face_value=F, coupon_rate=COUPON_RATE, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60]).calculate_price())
+        prices3.append(ConvertibleBond('', model=model, face_value=F, coupon_rate=COUPON_RATE, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60], gamma=gamma_).calculate_price())
+
+    ax[1].plot(gammas, prices1, label='callable')
+    ax[1].plot(gammas, prices2, label='mandatory')
+    ax[1].plot(gammas, prices3, label='vanilla')
+    ax[1].set_xlabel(r"Conversion rate $\gamma=\beta$")
+    #ax[1].xaxis.set_tick_params(labelsize=18)
+    #ax[1].yaxis.set_tick_params(labelsize=18)
+    ax[1].legend()
+    ax[1].grid() 
 
     prices = []
     call_price_rates = np.linspace(0.8, 10, num=1000) 
     for call_price_rate in call_price_rates:
         prices.append(callableCB('', model=model, face_value=F, coupon_rate=COUPON_RATE, coupon_dates=[6, 12, 18, 24, 30, 36, 42, 48, 54, 60], gamma=GAMMA, call_price=call_price_rate*F).calculate_price())
 
-    # plt.plot(call_price_rates, prices)
-    # plt.xlabel(r"Call price rate $\frac{F_c}{F}$")
-    # plt.ylabel("Initial price of the cCB")
-    # plt.grid()
-    #plt.show() 
+    ax[2].plot(call_price_rates, prices, label='callable')
+    ax[2].set_xlabel(r"Call price rate $\frac{F_c}{F}$")
+    #ax[2].xaxis.set_tick_params(labelsize=18)
+    #ax[2].yaxis.set_tick_params(labelsize=18)
+    ax[2].grid()
+    ax[2].legend()
+    plt.show()
+    plt.savefig('results/Analysis_5_4.png', dpi=200)
 
 
     # Question 5.5.
-    # P0 = 1000       # We want that each bond is initially worth F.
-    # cs = []
-    # gammas = np.arange(1, 7)
-    # for gamma_ in gammas:
-    #     cs.append(solve_for_c(model, P0, "cCB", 0, 0.2, 1000, gamma_))
+    P0 = 1000       # We want that each bond is initially worth F.
+    cs = []
+    cs1 = []
+    cs2 = []
 
-    # plt.bar(gammas, cs)
-    # plt.xlabel(r"Conversion rate $\gamma$")
-    # plt.ylabel("Coupon rate c")
-    # plt.grid()
-    #plt.show()  
+    gammas = np.arange(1, 7)
+    for gamma_ in gammas:
+        cs.append(solve_for_c(model, P0, "cCB", 0, 0.2, 200, gamma_))
+        cs1.append(solve_for_c(model, P0, "mCB", 0, 0.2, 200, gamma_))
+        cs2.append(solve_for_c(model, P0, "CB", 0, 0.2, 200, gamma_))
+   
+
+    f, ax = plt.subplots(nrows=1, ncols=1, figsize=(10,6))
+    width = 0.2
+    ax.bar(gammas-width, cs, width, label='callable')
+    ax.bar(gammas, cs1, width,label='mandatory')
+    ax.bar(gammas+width, cs2, width, label='vanilla')
+    ax.set_xlabel(r"Conversion rate $\gamma$")
+    ax.set_ylabel("Coupon rate c")
+    #ax.xaxis.set_tick_params(labelsize=18)
+    #ax.yaxis.set_tick_params(labelsize=18)
+    ax.grid()
+    ax.legend()
+    plt.show()
+    plt.savefig('results/Analysis_5_5.png', dpi=200)
     
-
 
     # Question 5.6.
     # nb_cs = 10
